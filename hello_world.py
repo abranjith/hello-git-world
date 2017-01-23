b0bda1cb3b27712e1da0dc5e42eb9a8588c16aa3
Ranjith Bojraj <Ranjith Bojraj>
2017-01-22 20:11:15 -0600

    first commit

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+import sys
+
+#python 3 print is a function, remember !!
+if (sys.version_info > (3, 0)):
+    print("Hello Git World !!") #lol, this also works in python2
+else:
+    print "Hello Git World !!"
